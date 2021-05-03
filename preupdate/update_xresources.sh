2d85acc071132878847da0d4d9e93b20791b7abb
takase1121 <20792268+takase1121@users.noreply.github.com>
2021-05-03 18:23:33 +0800

    fix xresources path

--- preupdate/update_xresources.sh
+++ preupdate/update_xresources.sh
@@ -2,5 +2,5 @@
 
 rm -f "$PSEUDO_HOME/.Xresources"
 for f in "$PSEUDO_HOME/.Xresources.d/"*; do
-	echo "#include \"Xresources.d/$(basename "$f" "$PSEUDO_HOME")\"" >> "$PSEUDO_HOME/.Xresources"
+	echo "#include \".Xresources.d/$(basename "$f" "$PSEUDO_HOME")\"" >> "$PSEUDO_HOME/.Xresources"
 done
